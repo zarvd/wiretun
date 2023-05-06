d99621ac2feae545407c12e2150d4d3ee3b1d468
lodrem <jialun.cai@pm.me>
2023-05-06 14:26:57 +0800

    Fix `add_duration` should add duration from that instant instead of now

--- src/device/time.rs
+++ src/device/time.rs
@@ -82,14 +82,13 @@ impl AtomicInstant {
 
     #[inline(always)]
     pub fn set_now(&self) {
-        self.d
-            .store(self.epoch.elapsed().as_millis() as _, Ordering::Relaxed);
+        let elpased = self.epoch.elapsed();
+        self.d.store(elpased.as_millis() as _, Ordering::Relaxed);
     }
 
     #[inline(always)]
     pub fn add_duration(&self, d: Duration) {
-        let d = (self.epoch.elapsed() + d).as_millis();
-        self.d.store(d as _, Ordering::Relaxed);
+        self.d.fetch_add(d.as_millis() as _, Ordering::Relaxed);
     }
 
     #[inline(always)]
@@ -164,7 +163,7 @@ mod tests {
         let now = now + Duration::from_secs(1);
         instant.add_duration(Duration::from_secs(1));
         assert_eq!(instant.to_std(), now);
-        assert!(instant.to_std() >= now);
+        assert!(instant.to_std() == now);
         assert!(instant.to_std() < now + Duration::from_secs(1));
         assert!(instant.to_std() > now - Duration::from_secs(1));
     }
